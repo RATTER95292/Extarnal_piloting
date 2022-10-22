5cc6790d7b64aa04310a291e836ab676535adfd5
RATTER95292 <sevenzee@mail.ru>
2022-10-22 14:01:14 +0300

    кар кар кар кар кар!!

--- app/src/main/java/com/example/extarnal_piloting/MainActivity.java
+++ app/src/main/java/com/example/extarnal_piloting/MainActivity.java
@@ -6,21 +6,45 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
+import android.annotation.SuppressLint;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.internal.NavigationMenu;
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
-    //ггг
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        //WebView sborka1 = (WebView)findViewById(R.id.video1);
+
+        //Самолет с ЭД
+        String video1 = "https://youtu.be/mGNPX4eOHGE";
+
+
+        //sborka1.getSettings().setJavaScriptEnabled(true);
+        //WebClient webViewClient = new WebClient();
+        //sborka1.setWebViewClient(webViewClient);
+
+        //sborka1.loadUrl(video1);
+
 
         DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
 
@@ -35,7 +59,27 @@ public class MainActivity extends AppCompatActivity {
 
         NavController navController = Navigation.findNavController(this, R.id.navHostFragment);
         NavigationUI.setupWithNavController(navigationView, navController);
+
     }
+
+    /*    Самолет с ЭД
+    сборка парашута https://youtu.be/BxuvOp4MLqo
+    построение плана полета https://youtu.be/gJREURn_vws
+
+      Самолет с ДВС
+    сборка https://youtu.be/jDsCSeW9DqU
+
+      Квадракоптера
+    сборка https://youtu.be/VHLuAjPhY5g
+
+      Обработка данных
+     Урок 1 https://youtu.be/BbEqox2ZmFc
+     Урок 2 https://youtu.be/_ZJT2A3jX-M?list=PLNLqotQpxb3nUX8nYVzU30YFJcoPCT5V0
+     Урок 3 https://youtu.be/vDmt0GCXKgs?list=PLNLqotQpxb3nUX8nYVzU30YFJcoPCT5V0
+     Урок 4 https://youtu.be/VKmomw1-4qE?list=PLNLqotQpxb3nUX8nYVzU30YFJcoPCT5V0
+     Урок 5 https://youtu.be/QIDTgRqqjic?list=PLNLqotQpxb3nUX8nYVzU30YFJcoPCT5V0
+    */
+
 
 
     public void Click(View view){
@@ -47,6 +91,7 @@ public class MainActivity extends AppCompatActivity {
                 intent.addCategory(Intent.CATEGORY_BROWSABLE);
                 intent.setData(Uri.parse("https://docs.google.com/document/d/1Khzio8HN-qFPduW5i2x3DC87ppNRm85uAzxq_BKNuRk/edit"));
                 startActivity(intent);
+                return;
 
             case R.id.primer1:
                 Intent intent1 = new Intent();
@@ -54,6 +99,7 @@ public class MainActivity extends AppCompatActivity {
                 intent1.addCategory(Intent.CATEGORY_BROWSABLE);
                 intent1.setData(Uri.parse("https://docs.google.com/document/d/12cleH8NUjDxZ20vS8ep84ZYLJ4dYM8LJPPEj5usdPwg/edit"));
                 startActivity(intent1);
+                return;
 
             case R.id.zadanie1:
 
@@ -62,6 +108,7 @@ public class MainActivity extends AppCompatActivity {
                 intent2.addCategory(Intent.CATEGORY_BROWSABLE);
                 intent2.setData(Uri.parse("https://docs.google.com/document/d/1QTzXn2_HCvx3uYZ9_dNkvzWXb4eSYvskahTKhjdGCwI/edit"));
                 startActivity(intent2);
+                return;
 
             case R.id.shablon2:
                 Intent intent3 = new Intent();
@@ -69,6 +116,7 @@ public class MainActivity extends AppCompatActivity {
                 intent3.addCategory(Intent.CATEGORY_BROWSABLE);
                 intent3.setData(Uri.parse("https://drive.google.com/file/d/1RUjgp4Fs3as682QDfMxIgifOHhA977vB/view"));
                 startActivity(intent3);
+                return;
 
             case R.id.primer2:
                 Intent inten = new Intent();
@@ -76,6 +124,7 @@ public class MainActivity extends AppCompatActivity {
                 inten.addCategory(Intent.CATEGORY_BROWSABLE);
                 inten.setData(Uri.parse("https://docs.google.com/document/d/1TJ7pT4_8BEuDRtimKAvlEZTw4jUljBSf2v-KdDwM4Tg/edit"));
                 startActivity(inten);
+                return;
 
                 // о корпорации
             case R.id.zadanie2:
@@ -85,6 +134,17 @@ public class MainActivity extends AppCompatActivity {
                 inte.setData(Uri.parse("https://docs.google.com/document/d/1QTzXn2_HCvx3uYZ9_dNkvzWXb4eSYvskahTKhjdGCwI/edit"));
                 startActivity(inte);
         }
+    }
+
+    public void Button(View view){
+
+
+
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+        intent.setData(Uri.parse("https://lahmeneffa.gitbook.io/docs-tkuik/"));
+        startActivity(intent);
     }
 
 
--- app/src/main/java/com/example/extarnal_piloting/processingFragment.java
+++ app/src/main/java/com/example/extarnal_piloting/processingFragment.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -34,7 +36,7 @@ public class processingFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment processingFragment.
+     * @return A new instance of fragment pilot_dvsFragment.
      */
     // TODO: Rename and change types and number of parameters
     public static processingFragment newInstance(String param1, String param2) {
@@ -49,6 +51,18 @@ public class processingFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        WebView sborka1 = (WebView) getView().findViewById(R.id.video1);
+        String video1 = "https://youtu.be/mGNPX4eOHGE";
+
+
+
+        sborka1.getSettings().setJavaScriptEnabled(true);
+        WebClient webViewClient = new WebClient();
+        sborka1.setWebViewClient(webViewClient);
+
+        sborka1.loadUrl(video1);
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
@@ -58,7 +72,16 @@ public class processingFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
+
+
         return inflater.inflate(R.layout.fragment_processing, container, false);
+    }
+
+    public class WebClient extends WebViewClient {
+        @Override
+        public  boolean shouldOverrideUrlLoading(WebView webView, String Url){
+            return false;
+        }
     }
 }
