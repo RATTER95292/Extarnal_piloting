fe94abaa099122c028f5c513249ca3be8422a6eb
Monte-Kristo <83087001+RATTER95292@users.noreply.github.com>
2022-11-26 23:05:46 +0300

    Изменил вид текста

--- app/src/main/java/com/example/extarnal_piloting/diagnosticFragment.java
+++ app/src/main/java/com/example/extarnal_piloting/diagnosticFragment.java
@@ -1,6 +1,8 @@
 package com.example.extarnal_piloting;
 
+import android.content.Intent;
 import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,6 +13,7 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -81,12 +84,37 @@ public class diagnosticFragment extends Fragment {
         ImageButton cc = view.findViewById(R.id.cc);
         ImageButton modem = view.findViewById(R.id.modem);
 
+        Button shablon = view.findViewById(R.id.shablon5);
+        Button primer = view.findViewById(R.id.primer5);
+
+        shablon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("https://drive.google.com/file/d/1fRRCGoMq-Q9SXbLKltZbKj513l6m2RzR/view?usp=sharing"));
+                startActivity(intent);
+            }
+        });
+
+        primer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("https://drive.google.com/file/d/1w0MXbDpFRwrWq4yOoeTgYT4TphZljZgp/view?usp=sharing"));
+                startActivity(intent);
+            }
+        });
+
         fuselage.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 if (event.getAction() == MotionEvent.ACTION_DOWN) {
                     textView.setText(R.string.fus);
-                }else {
+                }else{
                     textView.setText("");
                 }
                 return false;
